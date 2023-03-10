67a3130816fec5b16f64c961d59e511500a319b8
suibings <58060115+suibings@users.noreply.github.com>
2023-03-10 17:46:03 +0800

    修改

--- src/default.ts
+++ src/default.ts
@@ -1,12 +1,11 @@
 export const defaultSetting = {
   continuousDialogue: true,
   archiveSession: false,
-  openaiAPIKey: "",
+  openaiAPIKey: "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",
   openaiAPITemperature: 60,
   systemRule: ""
 }
 
 export const defaultMessage = `
-- 本站仅用于演示，填入自己的 key 才可使用。
 - 由 [碎冰] 搭建功能强大，响应速度快，欢迎自部署。
 - <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
