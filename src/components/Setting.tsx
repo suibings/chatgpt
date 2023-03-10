c14c69cbd3a430b4c4f8c09e480898fbddfe2af5
suibings <58060115+suibings@users.noreply.github.com>
2023-03-10 18:10:15 +0800

    Update Setting.tsx

--- src/components/Setting.tsx
+++ src/components/Setting.tsx
@@ -18,19 +18,6 @@ export default function Setting(props: {
   return (
     <div class="text-sm text-slate mb-2">
       <Show when={shown()}>
-        <SettingItem icon="i-carbon:api" label="OpenAI API Key">
-          <input
-            type="password"
-            value={props.setting().openaiAPIKey}
-            class="max-w-150px ml-1em px-1 text-slate rounded-sm bg-slate bg-op-15 focus:bg-op-20 focus:ring-0 focus:outline-none placeholder:text-slate-400 placeholder:op-30"
-            onInput={e => {
-              props.setSetting({
-                ...props.setting(),
-                openaiAPIKey: (e.target as HTMLInputElement).value
-              })
-            }}
-          />
-        </SettingItem>
         <SettingItem icon="i-carbon:user-online" label="系统角色指令">
           <input
             type="text"
