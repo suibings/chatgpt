d77a9c93357066d5238bff939daec97010fe0fdd
suibings <58060115+suibings@users.noreply.github.com>
2023-03-10 17:50:29 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -1,7 +1,7 @@
 export const defaultSetting = {
   continuousDialogue: true,
   archiveSession: false,
-  openaiAPIKey: "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",
+  openaiAPIKey: "",
   openaiAPITemperature: 60,
   systemRule: ""
 }
